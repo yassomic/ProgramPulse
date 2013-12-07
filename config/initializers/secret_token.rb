7e34faabd43cce0cf3323ba27568ad3def72fc35
MakerSquare Student <students@themakersquare.com>
2013-12-07 11:20:19 -0600

    rails_apps_composer: initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+ProgramPulse::Application.config.secret_key_base = '3a0e55df8c590fffe0d3a6d39941379e475411001951ff819eecc98f67622a44df798d5f8e8b36a040f9747fc264384a9e5fba080ce7bf1c173ec16ab1855405'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+ProgramPulse::Application.config.session_store :cookie_store, key: '_ProgramPulse_session'
