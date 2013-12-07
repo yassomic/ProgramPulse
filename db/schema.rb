3c47fd9f760cd240915dbfe55dd301e2c6fcdd7f
MakerSquare Student <students@themakersquare.com>
2013-12-07 14:11:16 -0600

    Add delimiter on school cost

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20131207195708) do
-
-  create_table "ratings", force: true do |t|
-    t.integer  "overall"
-    t.integer  "difficulty"
-    t.integer  "social_scene"
-    t.integer  "diversity"
-    t.integer  "location"
-    t.text     "comments"
-    t.integer  "user_id"
-    t.integer  "school_id"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
+ActiveRecord::Schema.define(version: 20131207173944) do
 
   create_table "schools", force: true do |t|
     t.string   "picture_url"
