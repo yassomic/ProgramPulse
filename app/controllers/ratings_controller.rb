56196d5fe33cf8f92607f3553a58f3f0e4b42761
MakerSquare Student <students@themakersquare.com>
2013-12-07 20:17:55 -0600

    Create ratings controller

---
+++ app/controllers/ratings_controller.rb
@@ -0,0 +1,11 @@
+class RatingsController < ApplicationController
+
+  def index
+    @ratings = Rating.all
+  end
+
+  def show
+    @rating = Rating.find(params[:id])
+  end
+
+end
--- app/models/school.rb
+++ app/models/school.rb
@@ -1,2 +1,4 @@
 class School < ActiveRecord::Base
+  belongs_to :user
+  has_many :ratings
 end
